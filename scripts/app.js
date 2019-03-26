01a89df45da081022ba741202ebead5581c0e8c8
morrisbc <31737772+morrisbc@users.noreply.github.com>
2019-03-25 22:37:29 -0400

    Fixed bug where searching by dex number would display the dex number rather than the name in the name field. Added support for deoxys forms via responsive dropdown menu similar to the buttons for nidoran

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,42 @@
 // Add event listeners
-addEventListener("DOMContentLoaded", getMon);
+addEventListener("DOMContentLoaded", getPokemon);
 document.getElementById("pokemon-name").addEventListener("keyup", addOptions);
-document.getElementById("get-mons").addEventListener("click", getMon);
+document.getElementById("get-mons").addEventListener("click", getPokemon);
 document.getElementById("btn-male").addEventListener("click", () => {
   document.getElementById("pokemon-name").value += "\u2642";
 });
 document.getElementById("btn-female").addEventListener("click", () => {
   document.getElementById("pokemon-name").value += "\u2640";
 });
+document
+  .getElementById("deoxys-normal")
+  .addEventListener("click", addDeoxysForm);
+document
+  .getElementById("deoxys-attack")
+  .addEventListener("click", addDeoxysForm);
+document
+  .getElementById("deoxys-defense")
+  .addEventListener("click", addDeoxysForm);
+document
+  .getElementById("deoxys-speed")
+  .addEventListener("click", addDeoxysForm);
 
-// Create the HTTP request object
-const http = new httpRequest();
+// Changes the text in the input field for deoxys variants depending on
+// the selection from the dropdown menu
+function addDeoxysForm(e) {
+  let name = document.getElementById("pokemon-name");
+  if (name.value.indexOf("-") === -1) {
+    name.value += `-${e.target.textContent.toLowerCase()}`;
+  } else {
+    name.value =
+      name.value.substring(0, name.value.indexOf("-")) +
+      `-${e.target.textContent.toLowerCase()}`;
+  }
+}
 
 // Gets the requested pokemon from the API and populates the page fields with
 // the information from the response
-function getMon(e) {
+function getPokemon(e) {
   // Get page elements to populate the data into
   let name = document.getElementById("pokemon-name").value;
   let output = document.getElementById("output");
@@ -31,11 +53,12 @@ function getMon(e) {
   }
 
   // Get the info from the API and display on screen
-  http.get(
+  httpGet(
     `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}/`,
-    (err, response) => {
+    response => {
+      console.log(response);
       // The HTTP request completed successfully
-      if (err === null) {
+      if (response !== null) {
         // Populate the pokedex number
         dexNum.innerText = `No. ${response.id}`;
 
@@ -43,6 +66,7 @@ function getMon(e) {
         front.setAttribute("src", `${response.sprites.front_default}`);
         back.setAttribute("src", `${response.sprites.back_default}`);
 
+        name = response.name;
         // Append the gender symbol back onto the end of the name for
         // nidoran
         if (name.startsWith("nidoran") && name.endsWith("-m")) {
@@ -74,11 +98,26 @@ function getMon(e) {
   e.preventDefault();
 }
 
+function httpGet(url, callback) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      let response = JSON.parse(xhr.responseText);
+      callback(response);
+    } else {
+      callback(null);
+    }
+  };
+  xhr.send();
+}
+
 // Adds special character options or prefix buttons for specific pokemon
 function addOptions(e) {
   // Get the male and female buttons for nidoran
   let maleBtn = document.getElementById("btn-male");
   let femaleBtn = document.getElementById("btn-female");
+  let deoxysMenu = document.getElementById("deoxysMenu");
 
   // Display the gender buttons when the user types "nidoran" and remove
   // them as soon as the input field no longer equals "nidoran" (case-insensitive)
@@ -88,5 +127,13 @@ function addOptions(e) {
   } else {
     maleBtn.style.display = "none";
     femaleBtn.style.display = "none";
+  }
+
+  // Show the drop down menu for deoxys forms once the user has
+  // typed "deoxys"
+  if (e.target.value.toLowerCase().startsWith("deoxys")) {
+    deoxysMenu.style.display = "inline";
+  } else {
+    deoxysMenu.style.display = "none";
   }
 }
