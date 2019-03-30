38821bcebbe5fc60213d4d327d34dd9e17fee289
morrisbc <31737772+morrisbc@users.noreply.github.com>
2019-03-30 01:15:58 -0400

    Refactored project. Split code bewteen two Javascript files. One for UI manipulation and one for main control flow. Added footer.

--- scripts/app.js
+++ scripts/app.js
@@ -1,87 +1,24 @@
+// Init UI Class
+const ui = new UI();
+
 // Add event listeners
-addEventListener("DOMContentLoaded", getResults);
-document
-  .getElementById("pokemon-name")
-  .addEventListener("keyup", addInputOptions);
 document.getElementById("get-mons").addEventListener("click", getResults);
-document
-  .getElementById("btn-male")
-  .addEventListener("click", appendGenderString);
-document
-  .getElementById("btn-female")
-  .addEventListener("click", appendGenderString);
-document
-  .getElementById("deoxys-normal")
-  .addEventListener("click", appendDeoxysFormString);
-document
-  .getElementById("deoxys-attack")
-  .addEventListener("click", appendDeoxysFormString);
-document
-  .getElementById("deoxys-defense")
-  .addEventListener("click", appendDeoxysFormString);
-document
-  .getElementById("deoxys-speed")
-  .addEventListener("click", appendDeoxysFormString);
-document.getElementById("chevron").addEventListener("click", e => {
-  if (e.target.nodeName === "A") {
-    if (e.target.className.indexOf("rotate-180") === -1) {
-      e.target.className += " rotate-180";
-    } else {
-      e.target.className = e.target.className.replace("rotate-180", "");
-    }
-  } else {
-    if (e.target.parentElement.className.indexOf("rotate-180") === -1) {
-      e.target.parentElement.className += " rotate-180";
-    } else {
-      e.target.parentElement.className = e.target.parentElement.className.replace(
-        "rotate-180",
-        ""
-      );
-    }
-  }
-});
-
-// Adds the gender symbol onto the end of the value in the input field
-function appendGenderString(e) {
-  {
-    let name = document.getElementById("pokemon-name");
-    if (name.value.endsWith("\u2642") || name.value.endsWith("\u2640")) {
-      name.value =
-        name.value.substring(0, name.value.length - 1) + e.target.value;
-    } else {
-      name.value += e.target.value;
-    }
-  }
-}
-
-// Changes the suffix of the value in the input field to the selection
-// chosen from the drop down menu
-function appendDeoxysFormString(e) {
-  let name = document.getElementById("pokemon-name");
-  if (name.value.indexOf("-") === -1) {
-    name.value += `-${e.target.textContent.toLowerCase()}`;
-  } else {
-    name.value =
-      name.value.substring(0, name.value.indexOf("-")) +
-      `-${e.target.textContent.toLowerCase()}`;
-  }
-}
+addEventListener("DOMContentLoaded", getResults);
 
 function getResults(e) {
-  let inputValue = document.getElementById("pokemon-name").value;
-  let output = document.getElementById("output");
-  let damageTypes = document.querySelector(".damage-types");
-  let hasLevitate = false;
+  let inputValue = document.getElementById("pokemon-name").value,
+    hasLevitate = false,
+    damages;
 
   // Make sure the name from the input field is acceptable for the API
-  inputValue = toAPIString(inputValue);
+  inputValue = ui.toAPIString(inputValue);
 
   fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue.toLowerCase()}/`)
     .then(pokemonResponse => {
       return pokemonResponse.json();
     })
     .then(pokemonData => {
-      getPokemon(pokemonData);
+      ui.populatePokemon(pokemonData);
       return pokemonData;
     })
     .then(pokemonData => {
@@ -94,143 +31,13 @@ function getResults(e) {
           fetch(type.type.url).then(typeResponse => typeResponse.json())
         )
       ).then(typeObjects => {
-        let damages = calculateTypeMatchups(typeObjects, hasLevitate);
-        console.log(damages);
-        damageTypes.innerHTML = "";
-        for (type in damages) {
-          damageTypes.innerHTML += `<span class="btn type ${type} damage-type d-flex justify-content-between align-items-center"><span>${type
-            .charAt(0)
-            .toUpperCase() +
-            type.slice(1)}</span><span class="btn bg-white damage-multiplier">${
-            damages[type]
-          }x</span></span>`;
-        }
+        damages = calculateTypeMatchups(typeObjects, hasLevitate);
+        ui.populateDamageTypes(damages);
       });
     })
-    .catch(() => (output.style.display = "none"));
+    .catch(ui.hideOutput.bind(ui));
 
   e.preventDefault();
-}
-
-function getPokemon(pokemon) {
-  // Get UI elements to populate the data into
-  let output = document.getElementById("output");
-  let nameHeading = document.getElementById("name-result");
-  let frontImg = document.getElementById("img-front");
-  let backImg = document.getElementById("img-back");
-  let types = document.getElementById("type-result");
-  let dexNum = document.getElementById("dex-num");
-  let height = document.getElementById("height-val");
-  let weight = document.getElementById("weight-val");
-
-  // Get the info from the API and display the info in the UI
-  console.log(pokemon);
-  // The HTTP request completed successfully
-  if (pokemon !== null) {
-    // Populate the pokedex number
-    dexNum.innerText = `No. ${pokemon.id}`;
-
-    // Populate the images, front and back, of the Pokemon
-    frontImg.setAttribute("src", `${pokemon.sprites.front_default}`);
-    backImg.setAttribute("src", `${pokemon.sprites.back_default}`);
-
-    let name = pokemon.name;
-
-    // Convert the pokemon name from the API response to one that looks
-    // better for the UI. Used for edge case pokemon with special names
-    // in the API database (ex. Nidoran)
-    name = toUIString(name);
-
-    // Populate the name of the Pokemon
-    nameHeading.innerText = `${name.charAt(0).toUpperCase() + name.slice(1)}`;
-
-    // Clear type bagdes from any previous queries and populate the page
-    // with type badges from the new query
-    types.innerHTML = "";
-    pokemon.types.forEach((monType, index) => {
-      types.innerHTML += `<span class="btn type ${
-        monType.type.name
-      }">${monType.type.name.charAt(0).toUpperCase() +
-        monType.type.name.slice(1)}</span>`;
-    });
-
-    // Clear height and weight from previous query
-    height.innerText = "";
-    weight.innerText = "";
-
-    // Update height and weight with new query values
-    height.innerText = `${pokemon.height / 10} m`;
-    weight.innerText = `${pokemon.weight / 10} kg`;
-
-    // Make the output area of the UI visible
-    output.style.display = "block";
-  } else {
-    // Hide the output area of the UI on error
-    output.style.display = "none";
-  }
-}
-
-// Adds UI elements for special input characters/substrings when the pokemon
-// they apply to is typed into the input field
-function addInputOptions(e) {
-  // Get the male and female button elements for nidoran genders
-  let maleBtn = document.getElementById("btn-male");
-  let femaleBtn = document.getElementById("btn-female");
-  // Get the drop down menu element for deoxys forms
-  let deoxysMenu = document.getElementById("deoxysMenu");
-
-  // Display the gender buttons when the user types "nidoran" and remove
-  // them as soon as the input field no longer equals "nidoran" (case-insensitive)
-  if (e.target.value.toLowerCase().startsWith("nidoran")) {
-    maleBtn.style.display = "inline";
-    femaleBtn.style.display = "inline";
-  } else {
-    maleBtn.style.display = "none";
-    femaleBtn.style.display = "none";
-  }
-
-  // Show the drop down menu for deoxys forms once the user has
-  // typed "deoxys" and remove it once the input field's value no
-  // longer begins with "deoxys" (case-insensitive)
-  if (e.target.value.toLowerCase().startsWith("deoxys")) {
-    deoxysMenu.style.display = "inline";
-  } else {
-    deoxysMenu.style.display = "none";
-  }
-}
-
-// Takes in a string from the UI input field that is unacceptable for
-// use in a call to the API and returns a new string that is acceptable
-// for use in an API call
-function toAPIString(uiString) {
-  let apiString;
-
-  if (uiString.startsWith("nidoran") && uiString.endsWith("\u2642")) {
-    apiString = uiString.replace("\u2642", "-m");
-  } else if (uiString.startsWith("nidoran") && uiString.endsWith("\u2640")) {
-    apiString = uiString.replace("\u2640", "-f");
-  } else {
-    apiString = uiString;
-  }
-
-  return apiString;
-}
-
-// Takes in a string from an API call response that is unacceptable
-// for display in the UI and returns a new string that is acceptable
-// for display in the UI
-function toUIString(apiString) {
-  let uiString;
-
-  if (apiString.startsWith("nidoran") && apiString.endsWith("-m")) {
-    uiString = apiString.replace("-m", "\u2642");
-  } else if (apiString.startsWith("nidoran") && apiString.endsWith("-f")) {
-    uiString = apiString.replace("-f", "\u2640");
-  } else {
-    uiString = apiString;
-  }
-
-  return uiString;
 }
 
 function calculateTypeMatchups(pokemonTypes, hasLevitate) {
---
+++ scripts/ui.js
@@ -0,0 +1,221 @@
+class UI {
+  constructor() {
+    // Get the page elements that will need to be manipulated
+    this.input = document.getElementById("pokemon-name");
+    this.output = document.getElementById("output");
+    this.nameHeading = document.getElementById("name-result");
+    this.frontImg = document.getElementById("img-front");
+    this.backImg = document.getElementById("img-back");
+    this.types = document.getElementById("type-result");
+    this.dexNum = document.getElementById("dex-num");
+    this.height = document.getElementById("height-val");
+    this.weight = document.getElementById("weight-val");
+    this.maleBtn = document.getElementById("btn-male");
+    this.femaleBtn = document.getElementById("btn-female");
+    this.deoxysMenu = document.getElementById("deoxysMenu");
+    this.damageTypes = document.querySelector(".damage-types");
+
+    // Add event listeners
+    document
+      .getElementById("pokemon-name")
+      .addEventListener("keyup", this.addInputOptions.bind(this));
+    document
+      .getElementById("btn-male")
+      .addEventListener("click", this.appendGenderString.bind(this));
+    document
+      .getElementById("btn-female")
+      .addEventListener("click", this.appendGenderString.bind(this));
+    document
+      .getElementById("deoxys-normal")
+      .addEventListener("click", this.appendDeoxysFormString.bind(this));
+    document
+      .getElementById("deoxys-attack")
+      .addEventListener("click", this.appendDeoxysFormString.bind(this));
+    document
+      .getElementById("deoxys-defense")
+      .addEventListener("click", this.appendDeoxysFormString.bind(this));
+    document
+      .getElementById("deoxys-speed")
+      .addEventListener("click", this.appendDeoxysFormString.bind(this));
+    document
+      .getElementById("chevron")
+      .addEventListener("click", this.rotateButton);
+  }
+
+  populatePokemon(pokemon) {
+    console.log(pokemon);
+    // Populate the pokedex number
+    this.dexNum.innerText = `No. ${pokemon.id}`;
+
+    // Populate the images, front and back, of the Pokemon
+    if (pokemon.sprites.front_default !== null) {
+      this.frontImg.setAttribute("src", `${pokemon.sprites.front_default}`);
+    } else {
+      this.frontImg.setAttribute(
+        "src",
+        "https://github.com/PokeAPI/sprites/blob/master/sprites/items/poke-ball.png?raw=true"
+      );
+    }
+    if (pokemon.sprites.back_default !== null) {
+      this.backImg.setAttribute("src", `${pokemon.sprites.back_default}`);
+    } else {
+      this.backImg.setAttribute(
+        "src",
+        "https://github.com/PokeAPI/sprites/blob/master/sprites/items/poke-ball.png?raw=true"
+      );
+    }
+
+    let name = pokemon.name;
+
+    // Convert the pokemon name from the API response to one that looks
+    // better for the UI. Used for edge case pokemon with special names
+    // in the API database (ex. Nidoran)
+    name = this.toUIString(name);
+
+    // Populate the name of the Pokemon
+    this.nameHeading.innerText = `${name.charAt(0).toUpperCase() +
+      name.slice(1)}`;
+
+    // Clear type bagdes from any previous queries and populate the page
+    // with type badges from the new query
+    this.types.innerHTML = "";
+    pokemon.types.forEach(monType => {
+      this.types.innerHTML += `<span class="btn type ${
+        monType.type.name
+      }">${monType.type.name.charAt(0).toUpperCase() +
+        monType.type.name.slice(1)}</span>`;
+    });
+
+    // Clear height and weight from previous query
+    this.height.innerText = "";
+    this.weight.innerText = "";
+
+    // Update height and weight with new query values
+    this.height.innerText = `${pokemon.height / 10} m`;
+    this.weight.innerText = `${pokemon.weight / 10} kg`;
+
+    this.showOutput();
+  }
+
+  populateDamageTypes(damages) {
+    this.damageTypes.innerHTML = "";
+    for (let type in damages) {
+      this.damageTypes.innerHTML += `<span class="btn type ${type} damage-type d-flex justify-content-between align-items-center"><span>${type
+        .charAt(0)
+        .toUpperCase() +
+        type.slice(1)}</span><span class="btn bg-white damage-multiplier">${
+        damages[type]
+      }x</span></span>`;
+    }
+  }
+
+  hideOutput() {
+    this.output.style.display = "none";
+  }
+
+  showOutput() {
+    this.output.style.display = "block";
+  }
+
+  // Takes in a string from an API call response that is unacceptable
+  // for display in the UI and returns a new string that is acceptable
+  // for display in the UI
+  toUIString(apiString) {
+    let uiString;
+
+    if (apiString.startsWith("nidoran") && apiString.endsWith("-m")) {
+      uiString = apiString.replace("-m", "\u2642");
+    } else if (apiString.startsWith("nidoran") && apiString.endsWith("-f")) {
+      uiString = apiString.replace("-f", "\u2640");
+    } else {
+      uiString = apiString;
+    }
+
+    return uiString;
+  }
+
+  // Takes in a string from the UI input field that is unacceptable for
+  // use in a call to the API and returns a new string that is acceptable
+  // for use in an API call
+  toAPIString(uiString) {
+    let apiString;
+
+    if (uiString.startsWith("nidoran") && uiString.endsWith("\u2642")) {
+      apiString = uiString.replace("\u2642", "-m");
+    } else if (uiString.startsWith("nidoran") && uiString.endsWith("\u2640")) {
+      apiString = uiString.replace("\u2640", "-f");
+    } else {
+      apiString = uiString;
+    }
+
+    return apiString;
+  }
+
+  // Adds UI elements for special input characters/substrings when the pokemon
+  // they apply to is typed into the input field
+  addInputOptions(e) {
+    // Display the gender buttons when the user types "nidoran" and remove
+    // them as soon as the input field no longer equals "nidoran" (case-insensitive)
+    if (e.target.value.toLowerCase().startsWith("nidoran")) {
+      this.maleBtn.style.display = "inline";
+      this.femaleBtn.style.display = "inline";
+    } else {
+      this.maleBtn.style.display = "none";
+      this.femaleBtn.style.display = "none";
+    }
+
+    // Show the drop down menu for deoxys forms once the user has
+    // typed "deoxys" and remove it once the input field's value no
+    // longer begins with "deoxys" (case-insensitive)
+    if (e.target.value.toLowerCase().startsWith("deoxys")) {
+      this.deoxysMenu.style.display = "inline";
+    } else {
+      this.deoxysMenu.style.display = "none";
+    }
+  }
+
+  // Changes the suffix of the value in the input field to the selection
+  // chosen from the drop down menu
+  appendDeoxysFormString(e) {
+    if (this.input.value.indexOf("-") === -1) {
+      this.input.value += `-${e.target.textContent.toLowerCase()}`;
+    } else {
+      this.input.value =
+        this.input.value.substring(0, this.input.value.indexOf("-")) +
+        `-${e.target.textContent.toLowerCase()}`;
+    }
+  }
+
+  // Adds the gender symbol onto the end of the value in the input field
+  appendGenderString(e) {
+    if (
+      this.input.value.endsWith("\u2642") ||
+      this.input.value.endsWith("\u2640")
+    ) {
+      this.input.value =
+        this.input.value.substring(0, this.input.value.length - 1) +
+        e.target.value;
+    } else {
+      this.input.value += e.target.value;
+    }
+  }
+
+  rotateButton(e) {
+    if (e.target.nodeName === "A") {
+      if (e.target.className.indexOf("rotate-180") === -1) {
+        e.target.className += " rotate-180";
+      } else {
+        e.target.className = e.target.className.replace("rotate-180", "");
+      }
+    } else {
+      if (e.target.parentElement.className.indexOf("rotate-180") === -1) {
+        e.target.parentElement.className += " rotate-180";
+      } else {
+        e.target.parentElement.className = e.target.parentElement.className.replace(
+          "rotate-180",
+          ""
+        );
+      }
+    }
+  }
+}
